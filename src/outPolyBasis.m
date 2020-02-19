0f91e53807177bf7681f6dfc8e0f474a1935723c
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2020-02-19 23:15:10 +0100

    bugfix: outPolyBasis

--- src/outPolyBasis.m
+++ src/outPolyBasis.m
@@ -7,21 +7,13 @@ function y = outPolyBasis(pBasis,n,t,showFig,symflag)
 % Author       : Wataru Ohnishi, the University of Tokyo, 2017
 %%%%%
 
-if nargin < 2
-    n = 1;
-end
-if nargin < 4
-    showFig = 0;
-end
-if nargin < 5
-    symflag = 0;
-end
-
+if nargin < 2, n = 1; end
+if nargin < 4, showFig = false; end
+if nargin < 5, symflag = false; end
 if ~iscell(pBasis) % single poly trajectories
     test = pBasis;
     pBasis = cell(1,1); pBasis{1} = test; clear test
 end
-
 if nargin < 3 || isempty(t)
     dT = pBasis{1}.BCt(2) - pBasis{1}.BCt(1);
     Ts = dT/1000;
@@ -29,55 +21,48 @@ if nargin < 3 || isempty(t)
 end
 
 npoly = length(pBasis); % number of trajectories
-norg = n;
 
 for k = 1:npoly
-    t_sym = sym('t_sym','real');
-    if norg > length(pBasis{k}.a_vpas(:,1)) % symbolic diff for high-order trajectory
-        T_sym = sym('t_sym_',[length(pBasis{k}.a_vpas(1,:)),1]);
-        for k2 = 1:length(T_sym)
-            T_sym(k2) = t_sym^(k2-1);
+    if n > length(pBasis{k}.a_vpas(:,1)) % symbolic diff for high-order trajectory
+        n_diff = n - length(pBasis{k}.a_vpas(:,1));
+        while n_diff > 0
+            pBasis{k}.a_vpas = [pBasis{k}.a_vpas; ...
+                polydiff(pBasis{k}.a_vpas(end,:));];
+            pBasis{k}.a_syms = [pBasis{k}.a_syms; ...
+                polydiff(pBasis{k}.a_syms(end,:));];
+            pBasis{k}.BC0 = [pBasis{k}.BC0;...
+                polyval(pBasis{k}.a_vpas(end,:),pBasis{k}.BCt(1))];
+            pBasis{k}.BC1 = [pBasis{k}.BC1;...
+                polyval(pBasis{k}.a_vpas(end,:),pBasis{k}.BCt(2))];
+            n_diff = n_diff - 1;
         end
-        y_sym = fliplr(pBasis{k}.a_vpas(1,:))*T_sym;
-        for kk = length(pBasis{k}.a_vpas(:,1))+1:norg
-            diff_y_sym = diff(y_sym,kk-1);
-            pBasis{k}.a_vpas = ...
-                [pBasis{k}.a_vpas;
-                zeros(1,length(pBasis{k}.a_vpas(1,:))-length(sym2poly(diff_y_sym))), sym2poly(diff_y_sym)];
-        end
-        n = length(pBasis{k}.a_vpas(:,1));
     end
 end
 
-nmax = max(cellfun(@(x)length(x.a_vpas),pBasis));
-T_sym2 = sym('t_sym_',[nmax,1]);
-for k = 1:length(T_sym2)
-    T_sym2(k) = t_sym^(k-1);
-end
-T_sym2 = flipud(T_sym2);
-
-[BCt,indices] = pBasisToBCt(pBasis,t);
-
+[BCt,t_segmentIdx] = pBasisToBCt(pBasis,t);
 
 if ~symflag % numeric answer
     y = zeros(1,length(t));
-    if n > length(pBasis{1}.BC0)
-        %             y(1:indices(1)-1) = polyval(pBasis{1}.a_vpas(n,:),t(indices(1)));
-        y(1:indices(1)-1) = 0;
-    else
-        y(1:indices(1)-1) = pBasis{1}.BC0(n);
-    end
-    for k = 1:length(BCt)-1
-        y(indices(k):indices(k+1)-1) = ...
-            polyval(double(pBasis{k}.a_vpas(n,:)),t(indices(k):indices(k+1)-1));
-    end
-    if n > length(pBasis{end}.BC1)
-        %             y(indices(end):end) = polyval(pBasis{end}.a_vpas(n,:),t(indices(end)));
-        y(indices(end):end) = 0;
-    else
-        y(indices(end):end) = pBasis{end}.BC1(n);
+    for k = 0:1:npoly+1
+        if k == 0
+            idx = t_segmentIdx == 0;
+            y(idx) = 0;
+        elseif k <= npoly
+            idx = t_segmentIdx == k;
+            y(idx) = polyval(double(pBasis{k}.a_vpas(n,:)),t(idx));
+        else
+            idx = t_segmentIdx == npoly+1;
+            y(idx) = pBasis{end}.BC1(n);
+        end
     end
 else % symbolic answer
+    nmax = max(cellfun(@(x)length(x.a_vpas),pBasis));
+    T_sym2 = sym('t_sym_',[nmax,1]);
+    for k = 1:length(T_sym2)
+        T_sym2(k) = t_sym^(k-1);
+    end
+    T_sym2 = flipud(T_sym2);
+    
     y = sym('y',[1,length(t)]);
     y(1:indices(1)-1) = pBasis{1}.BC0(n);
     for k = 1:length(BCt)-1
@@ -98,16 +83,11 @@ end
 end
 
 
-function [BCt,indices] = pBasisToBCt(pBasis,t)
+function [BCt,t_segmentIdx] = pBasisToBCt(pBasis,t)
 BCt = cellfun(@(x)x.BCt(1),pBasis);
-BCt = [BCt,pBasis{end}.BCt(end)];
+BCt = [BCt,pBasis{end}.BCt(end),inf]; % inf for final segment
 
-tmp = arrayfun(@(x)find(abs(x-t)<eps*5),BCt,'UniformOutput',false);
-if any(cellfun(@isempty,tmp))
-    warning('Sampled time vector does not contain boudary condition!');
-    indices = arrayfun(@(x)find(t<x,1,'last'),BCt)+1;
-else
-    indices = arrayfun(@(x)find(abs(x-t)<eps*5),BCt);
-end
+t_segmentIdx = arrayfun(@(x)find(x<BCt,1,'first'),t) - 1;
+
 end
 
--- src/polydiff.m
+++ src/polydiff.m
@@ -1,8 +1,8 @@
-function out = polydiff(in)
+function out = polydiff(in,n)
 %polydiff - calculate differentiated coeffs of polynomial
 %
-% out = polydiff(in)
-% example: 
+% out = polydiff(in,n)
+% example:
 % in = [a,b,c,d] (for polyval)
 %  y = a*t^3 + b*t^2 + c*t + d
 % dy = 3*a*t^2 + 2*b*t + c
@@ -11,12 +11,20 @@ function out = polydiff(in)
 % Author  : Wataru Ohnishi, The University of Tokyo, 2017
 %%%%%
 
+if nargin < 2, n = 1; end
+out = in;
+while n > 0
+    out = polydiff_main(out);
+    n = n -1;
+end
+end
+
+function out = polydiff_main(in)
 out = [0, in(1:end-1)];
 out = fliplr(out);
 for kk = 1:length(out)
-   out(kk) = out(kk)*kk; 
+    out(kk) = out(kk)*kk;
 end
 
 out = fliplr(out);
-
 end
