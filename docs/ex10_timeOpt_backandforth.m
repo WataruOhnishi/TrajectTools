39175465c2ec5d917de83500c52cfcd72a1f606a
WataruOhnishi <WataruOhnishi@users.noreply.github.com>
2020-04-26 11:28:19 +0200

    update ex10

--- docs/ex10_timeOpt_backandforth.m
+++ docs/ex10_timeOpt_backandforth.m
@@ -10,15 +10,15 @@ tfin_init = 5; % final time
 d = 5; % spline degree, minimum 3
 n = 32; % number of knots
 
-tfin_opt = tfin_init;
+tfin_feasible = tfin_init;
 tfin_infeasible = 0;
 tfin_diff_stop = 0.01;
 itr = 1;
 maxitr = 10;
 
 % tfin_opt minimization by bisection method
-while (abs(tfin_opt-tfin_infeasible)) > tfin_diff_stop && itr <= maxitr
-    tfin = (tfin_opt+tfin_infeasible)/2;
+while (abs(tfin_feasible-tfin_infeasible)) > tfin_diff_stop && itr <= maxitr
+    tfin = (tfin_feasible+tfin_infeasible)/2;
     
     % B-spline basis of degree d with n knots
     basis = BSplineBasis([0, tfin], d, n);
@@ -45,17 +45,17 @@ while (abs(tfin_opt-tfin_infeasible)) > tfin_diff_stop && itr <= maxitr
     sol = optimize(con, [], options);
     
     if sol.problem == 0 % feasible
-        tfin_opt = tfin;
-        fprintf('iteration %d (feasible): tfin_feasible=%.2f [s], tfin_infeasible =%.2f [s]\n',itr,tfin_opt,tfin_infeasible);
+        tfin_feasible = tfin;
+        fprintf('iteration %d (feasible): tfin_feasible=%.2f [s], tfin_infeasible =%.2f [s]\n',itr,tfin_feasible,tfin_infeasible);
     else
         tfin_infeasible = tfin;
-        fprintf('iteration %d (infeasible): tfin_feasible=%.2f [s], tfin_infeasible =%.2f [s]\n',itr,tfin_opt,tfin_infeasible);
+        fprintf('iteration %d (infeasible): tfin_feasible=%.2f [s], tfin_infeasible =%.2f [s]\n',itr,tfin_feasible,tfin_infeasible);
     end
     itr = itr + 1;
 end
 
 % B-spline basis of degree d with n knots
-basis = BSplineBasis([0, tfin_opt], d, n);
+basis = BSplineBasis([0, tfin_feasible], d, n);
 
 % scalar (1x1) spline variable
 pos = BSpline.sdpvar(basis, [1, 1]); % position trajectory
@@ -64,9 +64,9 @@ acc = pos.derivative(2); % acceleration trajectory
 jrk = pos.derivative(3); % jerk trajectory
 
 % equality constraints
-con = [pos.f(0) == 0, pos.f(tfin_opt) == pmax, ...
-    vel.f(0) == 0, vel.f(tfin_opt) == 0, ...
-    acc.f(0) == 0, acc.f(tfin_opt) == 0];
+con = [pos.f(0) == 0, pos.f(tfin_feasible) == pmax, ...
+    vel.f(0) == 0, vel.f(tfin_feasible) == 0, ...
+    acc.f(0) == 0, acc.f(tfin_feasible) == 0];
 %     jrk.f(0) == 0, jrk.f(tfin) == 0];
 
 % inequality constraints
@@ -81,7 +81,7 @@ if sol.problem == 1, error('infeasible! something wrong'); end
 
 
 %% Plot
-t = 0:tfin_opt/1000:tfin_opt;
+t = 0:tfin_feasible/1000:tfin_feasible;
 
 y1 = value(pos); y1 = y1.f(t);
 y2 = value(vel); y2 = y2.f(t);
